ff49ec2f0d1c7774eca3a5c47a9d6c829239e5b5
PREM RANJAN SHARMA <153382361+premranjansharma@users.noreply.github.com>
2026-05-12 13:57:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable*/
 import React, { useState, useEffect} from "react";
 import { Routes, Route, Link, useLocation,Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
