716f1da03782950fd10a793fc85db267713a2cad
PREM RANJAN SHARMA <153382361+premranjansharma@users.noreply.github.com>
2026-05-15 23:56:29 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const API = "http://localhost:5000/api";
+const API = process.env.REACT_APP_BASE_URL;
 
 export default function Profile() {
   const [user, setUser] = useState(null);
